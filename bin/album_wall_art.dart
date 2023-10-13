4ae9e6831ec0c6ff51b029aca3b2d860d9b5279e
Degover <chaors2000@gmail.com>
2023-10-13 17:00:19 -0300

    🎉 Created empty Dart project

---
+++ bin/album_wall_art.dart
@@ -0,0 +1,5 @@
+import 'package:album_wall_art/album_wall_art.dart' as album_wall_art;
+
+void main(List<String> arguments) {
+  print('Hello world: ${album_wall_art.calculate()}!');
+}
---
+++ test/album_wall_art_test.dart
@@ -0,0 +1,8 @@
+import 'package:album_wall_art/album_wall_art.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
